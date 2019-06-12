372d23734dd7dceda179520a271661636e29716e
Peter Styk <peter@styk.tv>
2019-06-12 02:26:22 +0100

    multiprocessing, standalone ok, ableton fail on flush

--- liveql.py
+++ liveql.py
@@ -2,43 +2,48 @@ import Live
 from _Framework.ControlSurface import ControlSurface # Central base class for scripts based on the new Framework
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
+sys.path.append('/Users/piotrek/Library/Python/2.7/lib/python/site-packages/multiprocessing')
 import os
-import threading
+import multiprocessing
 from serve import Serve
 #from serve_basic import ServeDebugGraphql
 import time
+import logging
+import sys
+
 
 
 class LiveQL(ControlSurface):
 
     serve = None
     thread_serve = None
+    logging = None
 
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
 
-        self.serve = Serve("Serve")
 
-        self.thread_serve = threading.Thread(target=self.serve.start_graphql_endpoint)
-        self.thread_serve.daemon = True
+
+        #self.serve = Serve("Serve")
+        #d = multiprocessing.Process(name='FlaskDaemon', initializer=mute, target=self.serve.start_graphql_endpoint)
+        #d.daemon = True
+        #d.start()
+
+        #self.serve = Serve("Serve")
+        #self.thread_serve = threading.Thread(target=self.serve.start_graphql_endpoint)
+        #self.thread_serve.daemon = True
+        #self.thread_serve.start()
+
+        self.thread_serve = JobProcess()
         self.thread_serve.start()
-
-
-
-
 
             #self.log_message(sys.path)
             #self.log_message("LOADING....")
 
-
             #t = self.getTracks()[0]
             #self.log_message("TRACK: {0}".format(t.name))
 
-
-
         self.getTracks()[0].clip_slots[0].create_clip(4)
-
-
 
 #    def getSet(self):
 #        return Live.Application.get_application().get_document()
@@ -46,4 +51,40 @@ class LiveQL(ControlSurface):
     def getTracks(self):
         return Live.Application.get_application().get_document().tracks
 
+    def is_extension(self):
+        return False
 
+
+class StreamToLogger(object):
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+    filename="threading.log",
+    filemode='a'
+)
+
+class JobProcess(multiprocessing.Process):
+    def __init__(self):
+        super(JobProcess, self).__init__()
+        self.s = Serve("Serve")
+        self.name = 'FlaskDaemon'
+
+
+    def run(self):
+        thread_logger = logging.getLogger(self.name)
+        sys.stdout = StreamToLogger(thread_logger, logging.INFO)
+        sys.stderr = StreamToLogger(thread_logger, logging.ERROR)
+        thread_logger.info("Starting " + self.name + "...")
+        self.s.start_graphql_endpoint()
--- standalone.py
+++ standalone.py
@@ -1,11 +1,66 @@
 from serve_basic import ServeDebugGraphql
 import time
 import threading
+import multiprocessing
+import logging
+import sys
 print "Starting GraphQL Endpoint: http://localhost:5000/graphql"
-s=ServeDebugGraphql("ServeBasic")
 
-t1 = threading.Thread(target=s.start_graphql_endpoint)
-t1.start()
+
+#s=ServeDebugGraphql("ServeBasic")
+#d = multiprocessing.Process(name='FlaskDaemon', target=s.start_graphql_endpoint)
+#d.daemon = True
+#d.start()
+
+
+
+#s=ServeDebugGraphql("ServeBasic")
+#t1 = threading.Thread(target=s.start_graphql_endpoint)
+#t1.start()
+
+
+
+
+class StreamToLogger(object):
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+    filename="threading.log",
+    filemode='a'
+)
+
+class JobProcess(multiprocessing.Process):
+    def __init__(self):
+        super(JobProcess, self).__init__()
+        self.s = ServeDebugGraphql("ServeBasiccc")
+        self.name = 'FlaskDaemon'
+
+
+    def run(self):
+        thread_logger = logging.getLogger(self.name)
+        sys.stdout = StreamToLogger(thread_logger, logging.INFO)
+        sys.stderr = StreamToLogger(thread_logger, logging.ERROR)
+        thread_logger.info("Starting " + self.name + "...")
+        self.s.start_graphql_endpoint()
+
+print "Notice how regular print statements don't go to the log."
+#testing_module = __import__("test_thread")
+
+test_job = JobProcess()
+test_job.start()
+print "Process queued!"
 
 x = True
 while x:
